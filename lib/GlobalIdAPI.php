6a2e9bf9f2c0742ca377771bd79b28aa2efc3e36
Andrew Guy <andy@torchlighttechnology.com>
2017-02-28 10:15:26 -0500

    initial commit of GlobalIdAPI class

---
+++ lib/GlobalIdAPI.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace torchlighttechnology\glid;
+
+use APIError;
+
+/**
+ * TTG Global ID PHP client
+ *
+ * @author guyandy
+ */
+
+class GlobalIdAPI
+{
+	protected $api_host;
+
+	public function __construct($api_host)
+	{
+		$this->api_host = $api_host;
+	}
+
+	/**
+	 * Lookup Email
+	 *
+	 * @return array API response object.
+	 */
+
+	public function email_lookup($email)
+	{
+		$request = [
+			'request' => 'email_lookup',
+			'email' => $email
+		];
+		return $this->api_request($request);
+	}
+
+	/**
+	 * Lookup Hash
+	 *
+	 * @return array API response object.
+	 */
+
+	public function hash_lookup($phone)
+	{
+		$request = [
+			'request' => 'hash_lookup',
+			'phone' => $phone
+		];
+		return $this->api_request($request);
+	}
+
+
+
+	protected function api_request($payload = null)
+	{
+		$ch = curl_init($this->api_host);
+		curl_setopt($ch, CURLOPT_POST, 1);
+		curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($paylod));
+		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($ch, CURLOPT_TIMEOUT, 15);
+		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+
+		$code = null;
+		try {
+			$result = curl_exec($ch);
+			$code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+			$response = json_decode($result);
+
+			if ($code != 200) {
+				throw new API_Error('Request was not successful', $code, $result, $response);
+			}
+		} catch (API_Error $e) {
+			$response = (object) array(
+				'code' => $code,
+				'status' => 'error',
+				'success' => false,
+				'exception' => $e
+			);
+		}
+
+		curl_close($ch);
+
+		return $response;
+	}
+}
